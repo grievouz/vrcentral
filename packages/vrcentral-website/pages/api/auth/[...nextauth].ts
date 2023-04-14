e6812580ff7112fc10122bf22d32a654094b1e9b
Grievouz <ronke.tammo@gmail.com>
2023-04-14 11:43:34 +0200

    move env vars into seperate files

--- packages/vrcentral-website/pages/api/auth/[...nextauth].ts
+++ packages/vrcentral-website/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { GITHUB_ID, GITHUB_SECRET } from "@app/env-server";
 
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
     }),
   ],
 };
+
 export default NextAuth(authOptions);
